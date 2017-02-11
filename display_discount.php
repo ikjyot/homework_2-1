62f167b682a014d31b0c99e38d946e41abe8658f
Ikjyot <isg6@njit.edu>
2017-02-10 23:53:18 -0500

    Modifies first 3 echo statements

--- display_discount.php
+++ display_discount.php
@@ -1,5 +1,5 @@
 <?php
-  $product_description = $_POST['product_decription'];
+  $product_description = $_POST['product_description'];
   $list_price = $_POST['list_price'];
   $discount_percentage = $_POST['discount_percent'];
 
@@ -17,13 +17,13 @@
     <main>
         <h1>This page is under construction</h1>
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_percentage; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo ''; ?></span><br>
